a061e8f3cdf3f99c4e431d4aa5a1dd54455c9d84
Marko Vukolic <vukolicm@gmail.com>
2017-04-06 23:41:27 +0200

    projects.hbs file added and modified server.js and header.hbs

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs')
+})
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: "Unable to handle request."
